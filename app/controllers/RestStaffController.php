604da0976004ce35f65f9f6524975c3ce60bfe8b
IMPACTOS DIGITALES <77515822+danjohn007@users.noreply.github.com>
2026-05-25 13:24:03 -0600

    fix: all staff access links append ?modo=staff to bypass comensal form

--- app/controllers/RestStaffController.php
+++ app/controllers/RestStaffController.php
@@ -31,7 +31,7 @@ class RestStaffController extends BaseController
         $staff = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         $restaurante = $this->restModel->find($restauranteId);
-        $linkAcceso  = BASE_URL . 'acceso/' . $restaurante['slug'];
+        $linkAcceso  = BASE_URL . 'acceso/' . $restaurante['slug'] . '?modo=staff';
         $flash       = $this->getFlash();
         $pageTitle   = 'Gestión de Staff';
         $activeMenu  = 'rest_staff';
